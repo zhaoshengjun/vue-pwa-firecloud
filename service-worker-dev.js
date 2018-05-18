b829cebd9f838d9b1b0576f1f8faa105a9c6c887
zhaoshengjun <zhao.shengjun@gmail.com>
2018-05-18 21:44:01 +1000

    add dev service worker

---
+++ service-worker-dev.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', () => {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', () => {
+  self.clients
+    .matchAll({
+      type: 'window'
+    })
+    .then(clients => {
+      for (let client of clients) {
+        client.navigate(client.url)
+      }
+    })
+})
